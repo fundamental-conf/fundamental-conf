7bbf09a9fd6c18b6307ca9e4aa35c1d00ab9ac91
droshev <mladen.droshev@sap.com>
2021-06-19 17:57:00 -0400

    docs: cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AgendaComponent } from './agenda/agenda.component';
-import { LandingComponent } from './landing/landing.component';
-
 const routes: Routes = [
-  { path: '', component: LandingComponent },
-  { path: 'agenda', component: AgendaComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { AgendaComponent } from './agenda/agenda.component';
-import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AgendaComponent,
-    LandingComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

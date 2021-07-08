313ba4183a445d79d2f06e8e5ad4ac31ad7b3d27
hanjunt <hanjunt@my.yorku.ca>
2021-07-08 15:48:52 -0400

    feat: landing page
    customize timezone, speaker section

--- src/app/desktop/desktop.component.ts
+++ src/app/desktop/desktop.component.ts
@@ -11,7 +11,14 @@ export class DesktopComponent implements OnInit {
   speakerData: any = (data as any).default;
   speakers: any[] = [];
   totalNumSpeaker: number = 0;
+
+  conferenceTime: string = '';
+  timezone: string = '';
+  startTimeUTC: number = 14;
+  endTimeUTC: number = 18;
+
   options = { threshold: 0.6 };
+  
 
   constructor() { }
 
@@ -19,6 +26,7 @@ export class DesktopComponent implements OnInit {
     this.speakers = this.speakerData.speakers;
     this.totalNumSpeaker = this.speakers.length;
     this.initializeAnimation();
+    this.customizeTime();
   }
   
   getImgSrc(name: string): string {
@@ -49,7 +57,6 @@ export class DesktopComponent implements OnInit {
           })
 
           const className = entry.target.className;
-          console.log(className);
           if (className === 'date' || className === 'speakers') {
             const activeSection = document.querySelector(`.${className}`) as HTMLElement;
             activeSection.classList.add('active');
@@ -61,4 +68,65 @@ export class DesktopComponent implements OnInit {
         }
     });
   }
+
+  customizeTime(): void {
+    // set up timezone
+    const offset = new Date().getTimezoneOffset();
+    if (offset > 0) {
+      this.timezone = timezoneMap1[offset];
+    } else {
+      this.timezone = timezoneMap2[-offset];
+    }
+    
+    // set up time
+    const hourDiff = offset / 60;
+    let startTag = '';
+    let endTag = '';
+    let localStart = this.startTimeUTC - hourDiff;
+    let localEnd = this.endTimeUTC - hourDiff;
+    if (localStart > 12) {
+      localStart = localStart % 12;
+      startTag = 'PM';
+    } else {
+      startTag = 'AM';
+    }
+    if (localEnd > 12) {
+      localEnd = localEnd % 12;
+      endTag = 'PM';
+    } else {
+      endTag = 'AM';
+    }
+    this.conferenceTime = localStart + ' ' + startTag + ' - ' + localEnd + ' ' + endTag;
+  }
+
 }
+
+const timezoneMap1: { [key: number]: string } = {
+  // GMT-7
+  420 : 'PDT',
+  // GMT-6
+  360 : 'CST',
+  // GMT-5
+  300 : 'EST',
+  // GMT-4
+  240 : 'EDT',
+  // GMT-3
+  180 : 'ADT',
+  // GMT-2
+  120 : 'GMT-2',
+  // GMT-1
+  60 : 'GMT-1'
+};  
+
+const timezoneMap2: { [key: number]: string } = {
+  // GMT
+  0 : 'WET',
+  // GMT+1
+  60 : 'CET',
+  // GMT+2
+  120 : 'EET',
+  // GMT+3
+  180 : 'EST',
+  // GMT+4
+  240 : 'MSK'
+}; 

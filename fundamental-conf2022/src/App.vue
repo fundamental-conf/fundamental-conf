b0063564eeeae29993310d80cc7047f1ab0c5bfd
ynnenu <enielav1997@hotmail.com>
2022-07-11 03:41:13 -0400

    added the call for content

--- fundamental-conf2022/src/App.vue
+++ fundamental-conf2022/src/App.vue
@@ -2,14 +2,17 @@
   <NavBar />
   <MainComponent />
   <FDAbout />
+  <FDCallForContent />
   <FDFooter />
 </template>
 
 <script>
 import NavBar from "./components/NavBar.vue";
 import MainComponent from "./components/MainComponent.vue";
-import FDFooter from "./components/FDFooter.vue";
 import FDAbout from "./components/FDAbout.vue";
+import FDCallForContent from "./components/FDCallForContent.vue";
+import FDFooter from "./components/FDFooter.vue";
+
 
 export default {
   name: "App",
@@ -17,6 +20,7 @@ export default {
     NavBar,
     MainComponent,
     FDAbout,
+    FDCallForContent,
     FDFooter,
   },
 };
--- fundamental-conf2022/src/components/FDAbout.vue
+++ fundamental-conf2022/src/components/FDAbout.vue
@@ -1,8 +1,9 @@
 <template>
-  <section class="fd_background_silver fd_container">
+<section class="fd_background_silver fd_container">
 <!--bubble left--> 
 <svg viewBox="0 0 494 998" fill="none" xmlns="http://www.w3.org/2000/svg" style ="
-height: 50vh; top: 40vh">
+height: 50vh; top: 40vh;  left: 0;
+    position: absolute;">
 <g style="mix-blend-mode:color-burn" filter="url(#filter0_f_58_644)">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M98.9802 847.805C253.839 822.462 304.824 662.153 335.352 521.638C362.457 396.881 358.573 257.219 241.863 190.142C112.228 115.636 -57.0352 136.004 -176.557 227.425C-309.958 329.462 -399.196 486.663 -334.942 631.334C-266.507 785.416 -82.5579 877.515 98.9802 847.805Z" fill="url(#paint0_linear_58_644)"/>
 </g>
@@ -20,8 +21,9 @@ height: 50vh; top: 40vh">
 </svg>
 
 <!--buble right-->
-<svg viewBox="0 0 772 625" fill="none" xmlns="http://www.w3.org/2000/svg" style ="height: 50vh;  position: absolute;
-    left: 111vh;">
+<svg viewBox="0 0 772 625" fill="none" xmlns="http://www.w3.org/2000/svg" style ="height: 50vh;  position: absolute;   justify-self: right;
+    align-self: self-start;;
+    right:0;">
 <g style="mix-blend-mode:color-burn" filter="url(#filter0_f_58_643)">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M860.522 295.503C915.402 172.706 793.362 40.9033 679.234 -64.7891C577.906 -158.628 447.178 -239.906 326.364 -199.176C192.17 -153.935 125.424 -24.1248 149.322 113.86C175.994 267.867 275.841 424.37 441.691 466.728C618.33 511.842 796.188 439.455 860.522 295.503Z" fill="url(#paint0_linear_58_643)"/>
 </g>
@@ -39,31 +41,90 @@ height: 50vh; top: 40vh">
 </svg>
 
 <div class = "fd_about">
+  <div class = "fd_about_header">
   <h4 class ="fd_about_tittle">join the innovation
   </h4>
   <div class = "fd_about_line"></div>
     <div class = "fd_about_element"> <h1 class="fd_date"> 09
 .29</h1></div>
-      <p class ="fd_paragraph">
+</div>
+    <p class ="fd_paragraph">
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
 Ut nunc justo libero erat nibh sit lectus eu bibendum.
  Vehicula magna non sem sagittis porta malesuada. 
  Eu ipsum tortor porta tincidunt. Nibh amet ac viverra tellus euismod id.
       </p>
-    
-  
 </div>
 
-<!-- <div class = "fd_about--uptodate">
-  <h4 class ="fd_about_tittle--right">stay up to date
+
+ <!-- <div class = "fd_about">
+  <div class = "fd_about_header--right">
+  <h4 class ="fd_about_tittle">stay up to date
   </h4>
   <div class = "fd_about_line"></div>
-    <div class = "fd_about_element--right"> </div>
-      <p class ="fd_paragraph--right fd_paragraph">
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-Ut nunc justo libero erat nibh sit lectus eu bibendum.
- Vehicula magna non sem sagittis porta malesuada. 
- Eu ipsum tortor porta tincidunt. Nibh amet ac viverra tellus euismod id.
+    <div class = "fd_about_element"><svg width="459" height="137" viewBox="0 0 459 137" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M119.876 62.7968C123.028 62.7971 125.584 65.3532 125.583 68.5058C125.582 78.6441 124.157 88.7647 121.322 98.7859C119.418 105.518 113.107 109.782 106.423 110.925C93.8554 113.076 81.1011 114.167 68.5 114.167C65.3474 114.167 62.7917 111.611 62.7917 108.458C62.7917 105.306 65.3474 102.75 68.5 102.75C80.4474 102.75 92.5577 101.715 104.498 99.6721C108.178 99.0424 109.958 97.0134 110.335 95.6799C112.893 86.6404 114.166 77.5631 114.167 68.5044C114.167 65.3518 116.723 62.7964 119.876 62.7968Z" fill="url(#paint0_linear_0_1)"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M119.876 74.2082C123.028 74.2079 125.584 71.6518 125.583 68.4992C125.582 58.3598 124.157 48.238 121.322 38.2155C119.418 31.483 113.108 27.2188 106.423 26.075C93.8555 23.9244 81.1012 22.8333 68.5 22.8333C65.3474 22.8333 62.7917 25.389 62.7917 28.5416C62.7917 31.6942 65.3474 34.2499 68.5 34.2499C80.4474 34.2499 92.5576 35.2849 104.497 37.3281C108.178 37.9579 109.958 39.9869 110.335 41.3212C112.893 50.362 114.166 59.4444 114.167 68.5044C114.167 71.657 116.723 74.2086 119.876 74.2082Z" fill="url(#paint1_linear_0_1)"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M17.1243 74.2082C13.9717 74.2079 11.4163 71.6518 11.4167 68.4992C11.4179 58.3598 12.8432 48.238 15.6784 38.2155C17.5816 31.483 23.8924 27.2188 30.5769 26.075C43.1445 23.9244 55.8988 22.8333 68.5 22.8333C71.6526 22.8333 74.2083 25.389 74.2083 28.5416C74.2083 31.6942 71.6526 34.2499 68.5 34.2499C56.5526 34.2499 44.4424 35.2849 32.5026 37.3281C28.8222 37.9579 27.0417 39.9869 26.6646 41.3212C24.107 50.362 22.8344 59.4406 22.8333 68.5006C22.8329 71.6532 20.2769 74.2086 17.1243 74.2082Z" fill="url(#paint2_linear_0_1)"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M17.1243 62.7916C13.9717 62.792 11.4163 65.3466 11.4167 68.4992C11.4179 78.6387 12.8432 88.7619 15.6784 98.7843C17.5816 105.517 23.8924 109.781 30.5769 110.925C43.1445 113.075 55.8988 114.167 68.5 114.167C71.6526 114.167 74.2083 111.611 74.2083 108.458C74.2083 105.306 71.6526 102.75 68.5 102.75C56.5526 102.75 44.4424 101.715 32.5026 99.6718C28.8222 99.042 27.0417 97.0129 26.6646 95.6786C24.107 86.6378 22.8344 77.5606 22.8333 68.5006C22.8329 65.348 20.2769 62.7912 17.1243 62.7916Z" fill="url(#paint3_linear_0_1)"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M54.0823 52.2275C55.7652 51.1874 57.8667 51.0928 59.6362 51.9776L82.4695 63.3942C84.4034 64.3612 85.625 66.3378 85.625 68.4999C85.625 70.6621 84.4034 72.6387 82.4695 73.6056L59.6362 85.0223C57.8667 85.907 55.7652 85.8125 54.0823 84.7724C52.3994 83.7323 51.375 81.895 51.375 79.9166V57.0833C51.375 55.1049 52.3994 53.2675 54.0823 52.2275Z" fill="url(#paint4_linear_0_1)"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M233.496 30.1114C236.811 25.6914 242.141 22.8333 249.479 22.8333C253.99 22.8333 258.092 25.1139 260.807 27.3311C261.772 28.1187 262.681 28.9938 263.479 29.9226L273.047 26.0957C275.579 25.0826 278.475 26.003 279.958 28.2926C281.441 30.5821 281.097 33.6008 279.136 35.4978L269.291 45.0256C269.374 46.1354 269.458 47.4865 269.458 48.5208C269.458 62.056 266.475 78.2711 257.796 91.2887C248.907 104.623 234.295 114.167 212.375 114.167C200.5 114.167 192.731 112.684 187.737 111.019C185.234 110.185 183.423 109.303 182.145 108.537C181.507 108.154 181.005 107.801 180.619 107.501C178.125 105.604 177.892 102.177 178.56 100.565C179.443 98.4324 181.525 97.0416 183.833 97.0416C194.348 97.0416 202.168 94.514 207.721 91.5918C204.4 91.3566 200.825 90.8592 197.824 89.8087C194.789 88.7466 189.542 86.0351 189.542 79.9166C189.542 76.764 192.097 74.2082 195.25 74.2082C195.62 74.2082 196.763 74.0096 198.757 73.3789C196.135 72.5741 193.506 71.5759 191.25 70.3353C188.367 68.7494 183.833 65.5118 183.833 59.9374C183.833 56.7848 186.389 54.2291 189.542 54.2291C189.671 54.2291 189.806 54.2271 189.946 54.2231C184.412 49.9985 178.125 43.3082 178.125 34.2499C178.125 31.7344 179.772 29.5155 182.179 28.7865C184.561 28.0653 187.132 28.9709 188.538 31.0172L188.55 31.0335C188.583 31.0775 188.661 31.1794 188.787 31.3314C189.04 31.6351 189.49 32.1397 190.167 32.7817C191.52 34.0628 193.798 35.9071 197.27 37.8011C203.47 41.1831 213.665 44.8232 229.5 45.5401V42.8124C229.5 39.1616 230.388 34.2547 233.496 30.1114Z" fill="url(#paint5_linear_0_1)"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M339.125 34.25C339.125 24.7921 346.792 17.125 356.25 17.125H424.75C434.208 17.125 441.875 24.7921 441.875 34.25V102.75C441.875 112.208 434.208 119.875 424.75 119.875H356.25C346.792 119.875 339.125 112.208 339.125 102.75V34.25ZM356.25 28.5417C353.097 28.5417 350.542 31.0974 350.542 34.25V102.75C350.542 105.903 353.097 108.458 356.25 108.458H424.75C427.903 108.458 430.458 105.903 430.458 102.75V34.25C430.458 31.0974 427.903 28.5417 424.75 28.5417H356.25Z" fill="url(#paint6_linear_0_1)"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M367.667 57.0833C370.819 57.0833 373.375 59.639 373.375 62.7917V91.3333C373.375 94.486 370.819 97.0417 367.667 97.0417C364.514 97.0417 361.958 94.486 361.958 91.3333V62.7917C361.958 59.639 364.514 57.0833 367.667 57.0833Z" fill="url(#paint7_linear_0_1)"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M367.667 54.2292C372.396 54.2292 376.229 50.3956 376.229 45.6667C376.229 40.9377 372.396 37.1042 367.667 37.1042C362.938 37.1042 359.104 40.9377 359.104 45.6667C359.104 50.3956 362.938 54.2292 367.667 54.2292Z" fill="url(#paint8_linear_0_1)"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M384.792 57.0833C387.944 57.0833 390.5 59.639 390.5 62.7917V91.3333C390.5 94.486 387.944 97.0417 384.792 97.0417C381.639 97.0417 379.083 94.486 379.083 91.3333V62.7917C379.083 59.639 381.639 57.0833 384.792 57.0833Z" fill="url(#paint9_linear_0_1)"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M379.083 78.6481C379.083 66.7382 388.738 57.0833 400.648 57.0833C410.807 57.0833 419.042 65.3184 419.042 75.4769V91.3333C419.042 94.486 416.486 97.0417 413.333 97.0417C410.181 97.0417 407.625 94.486 407.625 91.3333V75.4769C407.625 71.6236 404.501 68.5 400.648 68.5C395.043 68.5 390.5 73.0435 390.5 78.6481V91.3333C390.5 94.486 387.944 97.0417 384.792 97.0417C381.639 97.0417 379.083 94.486 379.083 91.3333V78.6481Z" fill="url(#paint10_linear_0_1)"/>
+<defs>
+<linearGradient id="paint0_linear_0_1" x1="65.0002" y1="141.5" x2="114" y2="26.9998" gradientUnits="userSpaceOnUse">
+<stop stop-color="#7B5CB2"/>
+<stop offset="1" stop-color="#69ADF8"/>
+</linearGradient>
+<linearGradient id="paint1_linear_0_1" x1="65.0002" y1="141.5" x2="114" y2="26.9998" gradientUnits="userSpaceOnUse">
+<stop stop-color="#7B5CB2"/>
+<stop offset="1" stop-color="#69ADF8"/>
+</linearGradient>
+<linearGradient id="paint2_linear_0_1" x1="65.0002" y1="141.5" x2="114" y2="26.9998" gradientUnits="userSpaceOnUse">
+<stop stop-color="#7B5CB2"/>
+<stop offset="1" stop-color="#69ADF8"/>
+</linearGradient>
+<linearGradient id="paint3_linear_0_1" x1="65.0002" y1="141.5" x2="114" y2="26.9998" gradientUnits="userSpaceOnUse">
+<stop stop-color="#7B5CB2"/>
+<stop offset="1" stop-color="#69ADF8"/>
+</linearGradient>
+<linearGradient id="paint4_linear_0_1" x1="65.0002" y1="141.5" x2="114" y2="26.9998" gradientUnits="userSpaceOnUse">
+<stop stop-color="#7B5CB2"/>
+<stop offset="1" stop-color="#69ADF8"/>
+</linearGradient>
+<linearGradient id="paint5_linear_0_1" x1="237.5" y1="149.5" x2="261.5" y2="22.9998" gradientUnits="userSpaceOnUse">
+<stop stop-color="#7B5CB2"/>
+<stop offset="1" stop-color="#69ADF8"/>
+</linearGradient>
+<linearGradient id="paint6_linear_0_1" x1="415" y1="143" x2="436.5" y2="2.99999" gradientUnits="userSpaceOnUse">
+<stop stop-color="#7B5CB2"/>
+<stop offset="1" stop-color="#69ADF8"/>
+</linearGradient>
+<linearGradient id="paint7_linear_0_1" x1="415" y1="143" x2="436.5" y2="2.99999" gradientUnits="userSpaceOnUse">
+<stop stop-color="#7B5CB2"/>
+<stop offset="1" stop-color="#69ADF8"/>
+</linearGradient>
+<linearGradient id="paint8_linear_0_1" x1="415" y1="143" x2="436.5" y2="2.99999" gradientUnits="userSpaceOnUse">
+<stop stop-color="#7B5CB2"/>
+<stop offset="1" stop-color="#69ADF8"/>
+</linearGradient>
+<linearGradient id="paint9_linear_0_1" x1="415" y1="143" x2="436.5" y2="2.99999" gradientUnits="userSpaceOnUse">
+<stop stop-color="#7B5CB2"/>
+<stop offset="1" stop-color="#69ADF8"/>
+</linearGradient>
+<linearGradient id="paint10_linear_0_1" x1="415" y1="143" x2="436.5" y2="2.99999" gradientUnits="userSpaceOnUse">
+<stop stop-color="#7B5CB2"/>
+<stop offset="1" stop-color="#69ADF8"/>
+</linearGradient>
+</defs>
+</svg>
+ </div>
+    </div>
+      <p class ="fd_paragraph ">
+
       </p>
      
   
@@ -83,68 +144,52 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.fd_background_silver {
-  background-color: $brand-color-silver;
+
 .fd_about {
 
-  width:70%;
   padding-top: 15vh;
-&--uptodate {
-   padding-top: 90vh;
+&--uptodated {
+  bottom: 20%;
 }
-  .fd_about_tittle {
-    position: absolute;
-    width: 437px;
-    height: 55px;
-    // left: 237px;
-    // top: 171px;
-    font-family: 'Ubuntu';
-    font-style: normal;
-    font-weight: 500;
-    font-size: 48px;
-    line-height: 55px;
-    display: flex;
-    align-items: center;
-    text-align: justify;
-    letter-spacing: 0.02em;
-    color: #2865BE;
+.fd_about_header{
+  display: flex;
+   -webkit-flex-direction: row; 
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
 
   &--right {
-width: 346px;
-height: 55px;
-left: 1213px;
-top: 817px;
-font-family: 'Ubuntu';
-font-style: normal;
-font-weight: 500;
-font-size: 48px;
-line-height: 55px;
-display: flex;
-align-items: center;
-text-align: justify;
-letter-spacing: 0.02em;
-
-color: #2865BE;
+flex-direction: row-reverse;
+ -webkit-flex-direction: row-reverse; 
   }
+  .fd_about_tittle {
+  
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 55px;
+    font-family: 'Ubuntu';
+    font-style: normal;
+    letter-spacing: 0.02em;
+    color: #2865BE;
+    order:1;
+    flex: 2;
+    padding-top: 3%;
   }
 
   .fd_about_line {
 
-    position: relative;
-    display:flex;
-    width: 509px;
-    height: 0px;
-    left:44vh;
-     top: 3vh;
-
-    border: 1px solid #2865BE;
+  
+    padding-top: 6%;
+    border-bottom: 1px solid #2865BE;
+    order:2;
+    flex: 1;
   }
   .fd_about_element
-  {  position: relative;
-  left: 90vh;
-top: -10vh;
-width: 342px;
-height: 470px;
+  { 
+ 
+
+order:3;
+flex:1;
 
 
     .fd_date {
@@ -161,29 +206,26 @@ height: 470px;
 
   color: #69ADF8;
 
-    }}
+    }}}
     .fd_paragraph {
 
-      width: 70vh;
-left: 5vh;
-top: -25vh;
-position: relative;
-font-family: 'Source Sans 3';
-font-style: normal;
-font-weight: 400;
-font-size: 24px;
-line-height: 34px;
-display: flex;
-align-items: center;
-text-align: justify;
-letter-spacing: 0.01em;
+   width: 70vh;
+    left: 5vh;
+    top: -37vh;
+    position: relative;
+    font-family: "Source Sans 3";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 34px;
+    text-align: justify;
+    letter-spacing: 0.01em;
+    color: #052E69;
 
-color: #052E69;
     }
 
   
 
 }
   
-}
 </style>

c58bb8ee4ed89a06322e87b6ac6a1e2268acdcea
Inna Atanasova <i.atanasova@sap.com>
2022-09-28 16:06:11 -0400

    minor css fix for live now label

--- fundamental-conf2022/src/views/FDAgenda.vue
+++ fundamental-conf2022/src/views/FDAgenda.vue
@@ -55,19 +55,21 @@
       />
 
       <!--Agenda Schedule Body-->
-      <ul class="fd-agenda-body" >
+      <ul class="fd-agenda-body">
         <li
           class="fd-agenda-body__row"
           v-for="el in agenda"
           :key="el.startTime"
           :class="{ live: el.live }"
-        > 
+        >
           <div class="fd-agenda-body__time-container">
             <div :class="{ timeFocus: el.live }">
-              <time class="fd-agenda-body__time"  >
+              <time class="fd-agenda-body__time">
                 {{ convertTime(el.startTime, eventTime) }}</time
               >
-              <div class="fd-agenda-body__live" v-if=el.live> LIVE NOW</div>
+              <div class="fd-agenda-body__live-wrap">
+                <div class="fd-agenda-body__live" v-if="el.live">LIVE NOW</div>
+              </div>
             </div>
           </div>
 
@@ -156,12 +158,20 @@
                   class="fd-agenda-calendar-box__line"
                   aria-hidden="true"
                 ></div>
-                <p class="fd-agenda-calendar-box__addToCal" v-if="el.live===false">ADD TO CALENDAR</p>
+                <p
+                  class="fd-agenda-calendar-box__addToCal"
+                  v-if="el.live === false"
+                >
+                  ADD TO CALENDAR
+                </p>
                 <ul
                   class="fd-agenda-calendar-box__buttons"
                   v-for="cal in el.calendars"
                 >
-                  <li class="fd-agenda-calendar-box__button" v-if="el.live===false">
+                  <li
+                    class="fd-agenda-calendar-box__button"
+                    v-if="el.live === false"
+                  >
                     <a
                       :href="
                         'data:text/calendar;charset=utf8,' +
@@ -172,13 +182,19 @@
                       >Outlook</a
                     >
                   </li>
-                  <li class="fd-agenda-calendar-box__button" v-if="el.live===false">
+                  <li
+                    class="fd-agenda-calendar-box__button"
+                    v-if="el.live === false"
+                  >
                     <a target="_blank" rel="noreferrer" :href="cal.google"
                       ><span v-html="svgs.calGoogle" aria-hidden="true"> </span
                       >Google</a
                     >
                   </li>
-                  <li class="fd-agenda-calendar-box__button" v-if="el.live===false">
+                  <li
+                    class="fd-agenda-calendar-box__button"
+                    v-if="el.live === false"
+                  >
                     <a
                       :href="
                         'data:text/calendar;charset=utf8,' +
@@ -189,19 +205,23 @@
                       >iCal</a
                     >
                   </li>
-                  <li  class="fd-agenda-calendar-box__button" v-if="el.live===true">
-                    <a target="_blank" rel="noreferrer" 
-                    href="https://sap-se.zoom.us/j/93979836058"
-                    
-                      ><span v-html="svgs.live" aria-hidden="true"> </span
-                      >Watch LIVE Now</a
+                  <li
+                    class="fd-agenda-calendar-box__button"
+                    v-if="el.live === true"
+                  >
+                    <a
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://sap-se.zoom.us/j/93979836058"
+                      ><span v-html="svgs.live" aria-hidden="true"> </span>Watch
+                      LIVE Now</a
                     >
                   </li>
                 </ul>
               </li>
             </ul>
-  
-        </div></li>
+          </div>
+        </li>
       </ul>
     </div>
 
@@ -240,8 +260,8 @@ export default {
       localTime: new Date().toString().match(/([A-Z]+[\+-][0-9]+.*)/)[1],
       filteredValue: "All",
       lastFocussedElementID: null,
-      date: '2022-09-29 07:05:00 -04:00',
-      now: Math.trunc((new Date()).getTime() / 1000)
+      date: "2022-09-29 07:05:00 -04:00",
+      now: Math.trunc(new Date().getTime() / 1000),
     };
   },
 
@@ -249,30 +269,33 @@ export default {
     this.lineup = agenda;
     this.formattedLineup = this.formatLineup();
 
+    let interval;
+    let timeNow = new Date().toISOString();
+    const startCounterTime = new Date(
+      "2022-09-29T07:05:00.000-04:00"
+    ).toISOString();
+    const endCounterTime = new Date(
+      "2022-09-29T16:00:00.000-04:00"
+    ).toISOString();
 
-        let interval;
-        let timeNow = new Date().toISOString();
-        const startCounterTime = new Date("2022-09-29T07:05:00.000-04:00").toISOString();
-        const endCounterTime = new Date("2022-09-29T16:00:00.000-04:00").toISOString();
-
-        if((timeNow > startCounterTime) && (timeNow <= endCounterTime)) {
-          interval = setInterval(() => {
-            timeNow = new Date().toISOString();
-            if(timeNow > endCounterTime) {
-              clearInterval(interval);
-              return;
-            }
-            this.updateLiveSession();
-          }, 30000)
+    if (timeNow > startCounterTime && timeNow <= endCounterTime) {
+      interval = setInterval(() => {
+        timeNow = new Date().toISOString();
+        if (timeNow > endCounterTime) {
+          clearInterval(interval);
+          return;
         }
-
-  
+        this.updateLiveSession();
+      }, 30000);
+    }
   },
 
   computed: {
     dateInMilliseconds() {
-      let germanyDate = this.date.toLocaleString("en-US", {timeZone: "Germany/Berlin"});
-      return Math.trunc(Date.parse(germanyDate) / 1000)
+      let germanyDate = this.date.toLocaleString("en-US", {
+        timeZone: "Germany/Berlin",
+      });
+      return Math.trunc(Date.parse(germanyDate) / 1000);
     },
     seconds() {
       return (this.dateInMilliseconds - this.now) % 60;
@@ -285,7 +308,7 @@ export default {
     },
     days() {
       return Math.trunc((this.dateInMilliseconds - this.now) / 60 / 60 / 24);
-    }
+    },
   },
 
   //filters the posts/presentations of agenda
@@ -380,7 +403,6 @@ export default {
           .toISO({ suppressMilliseconds: true });
         let time = date.substring(date.indexOf("T") + 1);
 
-
         return (
           time.split(":")[0].replace(/^0+/, "0") + ":" + time.split(":")[1]
         );
@@ -389,17 +411,17 @@ export default {
       }
     },
     updateLiveSession() {
-      return this.formattedLineup.map(agenda => {
+      return this.formattedLineup.map((agenda) => {
         let timeNow = new Date().toISOString();
         let sessionTimeStart = new Date(agenda.startTime).toISOString();
         let sessionTimeEnd = new Date(agenda.endTime).toISOString();
 
-        if(timeNow > sessionTimeStart && timeNow < sessionTimeEnd) {
+        if (timeNow > sessionTimeStart && timeNow < sessionTimeEnd) {
           agenda.live = true;
         } else {
           agenda.live = false;
         }
-      })
+      });
     },
 
     //add to calendar function
@@ -575,39 +597,40 @@ export default {
 }
 
 .fd-agenda-body__row.live {
-
   box-sizing: border-box;
-/* Auto layout */
-display: flex;
-flex-direction: column;
-padding:1rem;
-gap: 24px;
-height:auto;
-width:auto;
+  /* Auto layout */
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  gap: 24px;
+  height: auto;
+  width: auto;
 
+  background: rgba(255, 255, 255, 0.3);
+  /* Nav Background Blur */
+  backdrop-filter: blur(4px);
+  /* Note: backdrop-filter has minimal browser support */
+  border-radius: 16px;
+  align-items: center;
+  align-content: center;
+  /* Inside auto layout */
 
-background: rgba(255, 255, 255, 0.3);
-/* Nav Background Blur */
-backdrop-filter: blur(4px);
-/* Note: backdrop-filter has minimal browser support */
-border-radius: 16px;
-align-items: center;
-align-content: center;
-/* Inside auto layout */
-
-border: 1px solid;
+  border: 1px solid;
   border-image-slice: 1;
   border-width: 1px;
-border-image-source: linear-gradient(-33deg, #7b5cb2, #7b5cb2, #69adf8, #82deff);
-
+  border-image-source: linear-gradient(
+    -33deg,
+    #7b5cb2,
+    #7b5cb2,
+    #69adf8,
+    #82deff
+  );
 }
 div.timeFocus {
   align-items: center;
   flex-direction: column;
   display: flex;
-  
-    
-  }
+}
 
 .fd-agenda-body {
   display: flex;
@@ -660,7 +683,6 @@ div.timeFocus {
 
     div {
       padding: 0.5rem;
-
     }
   }
 
@@ -834,29 +856,43 @@ div.timeFocus {
     border: 2px solid;
     border-image-slice: 1;
     border-width: 3px;
-    border-image-source: linear-gradient(-33deg, #7b5cb2, #7b5cb2, #69adf8, #82deff);
+    border-image-source: linear-gradient(
+      -33deg,
+      #7b5cb2,
+      #7b5cb2,
+      #69adf8,
+      #82deff
+    );
 
-/* Inside auto layout */
-flex: none;
-order: 1;
-flex-grow: 0;
+    /* Inside auto layout */
+    flex: none;
+    order: 1;
+    flex-grow: 0;
 
-font-family: 'Ubuntu';
-font-style: normal;
-font-weight: 700;
-font-size: 16px;
-line-height: 18px;
-display: flex;
-align-items: center;
-text-align: center;
-letter-spacing: 0.05em;
+    font-family: "Ubuntu";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 18px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    letter-spacing: 0.05em;
 
-/* Gradient 3 (Test) */
-background: linear-gradient(73.81deg, #7843D5 0.22%, #1DC4FF 99.78%);
--webkit-background-clip: text;
--webkit-text-fill-color: transparent;
-background-clip: text;
-text-fill-color: transparent;
+    /* Gradient 3 (Test) */
+    background: linear-gradient(73.81deg, #7843d5 0.22%, #1dc4ff 99.78%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    text-fill-color: transparent;
+  }
+
+  &__live-wrap {
+    margin-top: 1rem;
+    background: #ECF3FE;
+    width: fit-content;
+    height: fit-content;
+    padding: 0;
   }
 }
 
@@ -939,7 +975,7 @@ text-fill-color: transparent;
       align-items: center;
       text-align: center;
       letter-spacing: 0.05em;
-      color: #3E86EF;
+      color: #3e86ef;
       position: relative;
       border: 0.125rem solid transparent;
       border-radius: 0.25rem;
@@ -948,17 +984,17 @@ text-fill-color: transparent;
       transition: all 0.25s ease-in;
 
       &:hover {
-        color: #2865BE;
+        color: #2865be;
       }
 
       &:active {
-        color: #052E69;
+        color: #052e69;
       }
 
       &:focus {
         outline: none;
-        color: #3E86EF;
-        border-color: #7352AD;
+        color: #3e86ef;
+        border-color: #7352ad;
       }
     }
 
@@ -969,49 +1005,19 @@ text-fill-color: transparent;
       justify-content: center;
       align-items: center;
       margin-right: 0.5rem;
-      
+
       svg {
         height: 1.5rem;
       }
     }
   }
- div.timeFocus {
-  align-items: center;
-  gap:1rem;
- }
- .fd-agenda-body__row.live{
-  gap: 60px;
- }
-  .fd-agenda-body {
-      &__live {
-        transform: translate(55%, 0);
-    border-radius: 6px;
-    border: 2px solid;
-    border-image-slice: 1;
-    border-width: 3px;
-    border-image-source: linear-gradient(-33deg, #7b5cb2, #7b5cb2, #69adf8, #82deff);
-
-/* Inside auto layout */
-flex: none;
-order: 1;
-flex-grow: 0;
-
-font-family: 'Ubuntu';
-font-style: normal;
-font-weight: 700;
-font-size: 30px;
-line-height: 36px;
-display: flex;
-align-items: center;
-text-align: center;
-letter-spacing: 0.15em;
-
-/* Gradient 3 (Test) */
+  div.timeFocus {
+    align-items: center;
+    gap: 1rem;
   }
+  .fd-agenda-body__row.live {
+    gap: 60px;
   }
-  
-
-    
 }
 
 @media (min-width: 1000px) {
@@ -1044,7 +1050,7 @@ letter-spacing: 0.15em;
       right: 0;
     }
   }
- div.timeFocus {
+  div.timeFocus {
     text-align: center;
   }
   .fd-agenda-body {
@@ -1111,7 +1117,6 @@ letter-spacing: 0.15em;
 
       div {
         padding: 0;
-     
       }
     }
 
@@ -1199,6 +1204,44 @@ letter-spacing: 0.15em;
       text-transform: uppercase;
       color: #2865be;
     }
+
+    .fd-agenda-body {
+    &__live {
+      transform: translate(55%, 0);
+      border-radius: 6px;
+      border: 2px solid;
+      border-image-slice: 1;
+      border-width: 3px;
+      border-image-source: linear-gradient(
+        -33deg,
+        #7b5cb2,
+        #7b5cb2,
+        #69adf8,
+        #82deff
+      );
+
+      /* Inside auto layout */
+      flex: none;
+      order: 1;
+      flex-grow: 0;
+
+      font-family: "Ubuntu";
+      font-style: normal;
+      font-weight: 700;
+      font-size: 30px;
+      line-height: 36px;
+      display: flex;
+      align-items: center;
+      text-align: center;
+      letter-spacing: 0.15em;
+
+      /* Gradient 3 (Test) */
+    }
+
+    &__live-wrap {
+      margin-top: 0;
+    }
+  }
   }
 
   .fd-agenda-calendar-box {
